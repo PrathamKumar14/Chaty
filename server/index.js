5bc77ae6bc070881e55a4248b35cdde26b4e8591
Pratham kumar <prathamsolera@gmail.com>
2020-08-01 00:58:01 +0530

    emit message to client-side

--- server/index.js
+++ server/index.js
@@ -4,12 +4,46 @@ const http = require("http");
 
 const router = require("./router");
 
+const {
+  changeNameAndRoom,
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./users.js");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("admit", ({ name, room }) => {
+    console.log(name, room);
+
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) {
+      return callback(error);
+    }
+
+    socket.emit("message", { user: "admin", text: `Welcome, ${user.name}!` });
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      text: `We have a new hacker ${user.name} in the group!`,
+    });
+
+    socket.join(user.room);
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
