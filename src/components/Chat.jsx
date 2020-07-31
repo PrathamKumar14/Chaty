283fbd69fb340df84a5b67a19c08628a5e0a7f68
Pratham kumar <prathamsolera@gmail.com>
2020-07-31 23:05:10 +0530

    send name and room to server

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 import queryString from "query-string";
-import io, { Socket } from "socket.io-client";
+import io from "socket.io-client";
 
 var socket;
 
-function Chat(props) {
+function Chat({ location }) {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
-    const data = queryString.parse(props.location.search);
+    const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
-    setName(data.name);
-    setRoom(data.room);
+    setName(name);
+    setRoom(room);
 
-    socket.emit("join", { name, room });
-  }, [ENDPOINT, props.location.search]);
+    socket.emit("admit", { name, room });
+  }, [ENDPOINT, location.search]);
 
   return <h1>Chat</h1>;
 }
