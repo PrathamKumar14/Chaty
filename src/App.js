cb79fe0a16a75fab5cd9a7722e83eab467716332
Pratham kumar <prathamsolera@gmail.com>
2020-08-01 18:19:47 +0530

    Import all component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Chat from "./components/Chat";
+
 function App() {
-  return <h1>START</h1>;
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
 }
 
 export default App;
